388ad05b5b374ab6c317deda46d48a5a80efb6d7
daydream-skybreak <samueldesta323@gmail.com>
2025-10-02 16:43:47 +0300

    fix: modified seed data

--- CarRental/Services/AppDbContext.cs
+++ CarRental/Services/AppDbContext.cs
@@ -16,15 +16,15 @@ namespace CarRental.Services
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<UserModel>().HasData(
-                new UserModel { Id = 1, Name = "Alice Johnson", Email = "alice@example.com", Password = "Password123!", IsAdmin = true },
-                new UserModel { Id = 2, Name = "Bob Smith", Email = "bob@example.com", Password = "Password123!", IsAdmin = false },
-                new UserModel { Id = 3, Name = "Charlie Brown", Email = "charlie@example.com", Password = "Password123!", IsAdmin = false }
+                new UserModel { Id = 1, Name = "Alice Johnson", Email = "alice@example.com", Password = "12344321", IsAdmin = true },
+                new UserModel { Id = 2, Name = "Bob Smith", Email = "bob@example.com", Password = "43211234", IsAdmin = false },
+                new UserModel { Id = 3, Name = "Charlie Brown", Email = "charlie@example.com", Password = "12123434", IsAdmin = false }
             );
 
             modelBuilder.Entity<CarModel>().HasData(
-                new CarModel { Id = 1, Make = "Toyota", Model = "Camry", Year = 2021, Price = 50.0m, ImageUrl = "https://example.com/toyota_camry.jpg", AdminId = 1 },
-                new CarModel { Id = 2, Make = "Honda", Model = "Civic", Year = 2020, Price = 45.0m, ImageUrl = "https://example.com/honda_civic.jpg", AdminId = 1 },
-                new CarModel { Id = 3, Make = "Ford", Model = "Mustang", Year = 2019, Price = 70.0m, ImageUrl = "https://example.com/ford_mustang.jpg", AdminId = 1 }
+                new CarModel { Id = 1, Make = "Toyota", Model = "Camry", Year = 2021, Price = 50.0m, ImageUrl = "/images/honda_civic.jpeg", AdminId = 1 },
+                new CarModel { Id = 2, Make = "Honda", Model = "Civic", Year = 2020, Price = 45.0m, ImageUrl = "/images/toyota_camri.jpeg", AdminId = 1 },
+                new CarModel { Id = 3, Make = "Ford", Model = "Mustang", Year = 2019, Price = 70.0m, ImageUrl = "/images/ford_mustang.jpeg", AdminId = 1 }
             );
 
             modelBuilder.Entity<InquiresModel>().HasData(
